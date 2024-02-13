27c62ad4013bc45765b0e4815fe8733cc28aef93
Hotpotsss <ryanlovescod@googlemail.com>
2024-02-13 18:14:40 +0100

    s

--- game.js
+++ game.js
@@ -3,42 +3,80 @@ const optionButtonsElement = document.getElementById('option-buttons')
 
 let state = {}
 
-const narrativeTexts = [
+const textNodes = [
     {
         id: 1,
         text: 'You wake up in a strange place and see a jar of blue goo near you.',
         options: [
             {
-                text: 'Take the goo',
+                text: 'Take the blue goo',
                 setState: {blueGoo: true},
                 nextText: 2
             },
             {
-                text: 'Leave the goo',
+                text: 'Leave the blue goo',
                 nextText: 2
             }
         ]
     },
     {
-        id: 2
+        id: 2,
+        text: 'You venture forth in search of answers to where you are when you come across a merchant.',
+        options: [
+            {
+                text: 'Trade the blue goo for a sword',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: {blueGoo: false, sword: true},
+                nextText: 3
+            },
+            {
+                text: 'Trade the blue goo for a shield',
+                requiredState: (currentState) => currentState.blueGoo,
+                setState: {blueGoo: false, shield: true},
+                nextText: 3
+            },
+            {
+                text: 'Ignore the merchant',
+                nextText: 3
+            }
+        ]
     }
 ]
 
+//A function which will be called when the game is started.
 function startGame () {
     state = {}
-    showNarrativeText(1)
+    showTextNode(1)
 }
 
-function showNarrativeText (textIndex) {
-    const narrativeText = narrativeText.find(narrativeText => narrativeText.id === textIndex)
-    textElement.innerText = narrativeText.text
+function showTextNode (textNodeIndex) {
+    const textNode = textNodes.find(textNode => textNode.id === textNodeIndex)
+    textElement.innerText = textNode.text
+    //Remove Option Buttons
     while (optionButtonsElement.firstChild) {
         optionButtonsElement.removeChild(optionButtonsElement.firstChild)
     }
+
+    textNode.options.forEach(option => {
+        if (showOption(option)) {
+            const button = document.createElement('button')
+            button.innerText = option.text
+            button.classList.add('btn')
+            button.addEventListener('click', () => selectOption(option))
+            optionButtonsElement.appendChild(button)
+        }
+    })
+}
+
+function showOption(option) {
+    return option.requiredState == null || option.requiredState(state)
 }
 
 function selectOption (option) {
-
+    const nextTextNodeID = option.nextText
+    state = Object.assign(state, option.setState)
+    showTextNode(nextTextNodeID)
 }
 
+//Call startGame function to initiate game.
 startGame()
