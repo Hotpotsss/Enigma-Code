c667eff8deaa7811de128c931b31e476ddef7f30
Hotpotsss <ryanlovescod@googlemail.com>
2024-02-14 13:09:36 +0100

    d

---
+++ index.js
@@ -0,0 +1,11 @@
+
+
+//Instantiate textElement variable and assign the variable to the "narrativeText" HTML text element.
+const textElement = document.getElementById('narrativeText')
+document.getElementById('myButton').onclick = location.href = 'C:\Users\ryana\OneDrive\Documents\Enigma Code\game.html';
+
+/*<script type="text/javascript">
+    document.getElementById("myButton").onclick = function () {
+        location.href = "www.yoursite.com";
+    };
+</script>*/
