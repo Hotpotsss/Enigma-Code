44d88b4a09878872d01da8512f0cf2802958e5a3
Hotpotsss <ryanlovescod@googlemail.com>
2024-02-14 17:35:24 +0100

    V 0.1

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 
 
-//Instantiate textElement variable and assign the variable to the "narrativeText" HTML text element.
-const textElement = document.getElementById('narrativeText')
-document.getElementById('myButton').onclick = location.href = 'C:\Users\ryana\OneDrive\Documents\Enigma Code\game.html';
+///Instantiate optionButtonsElement variable and assign the variable to the "option-buttons" HTML container element.
+const playButtonElement = document.getElementById('option-buttons')
+//document.getElementById('option-buttons').onclick = location.href = 'https://www.youtube.com/watch?v=R1S_NhKkvGA';
+
+/*C:\Users\ryana\OneDrive\Documents\Enigma Code\game.html';*/
 
 /*<script type="text/javascript">
     document.getElementById("myButton").onclick = function () {
         location.href = "www.yoursite.com";
     };
 </script>*/
+
+function load_game() {
+    button.addEventListener('click') 
+    document.getElementById("content").innerHTML='<object type="text/html" data="game.html" ></object>';
+}
